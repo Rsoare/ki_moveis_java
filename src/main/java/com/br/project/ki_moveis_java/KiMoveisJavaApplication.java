dd4b59f1170cd2b993c66fc75bceadbe209ae89c
Rihan soares <rihan145@gmail.com>
2023-07-12 16:43:13 -0300

    fist commit

---
+++ src/main/java/com/br/project/ki_moveis_java/KiMoveisJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.project.ki_moveis_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiMoveisJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiMoveisJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/project/ki_moveis_java/KiMoveisJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.project.ki_moveis_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiMoveisJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
